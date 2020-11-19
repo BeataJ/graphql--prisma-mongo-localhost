288e954104f9fb9df69f25f00ae992f7551eae4a
beata <beata@emc2wave.com>
2020-11-18 19:24:09 -0800

    create prisma mutation for updatePost

--- src/prisma.js
+++ src/prisma.js
@@ -13,25 +13,46 @@ const prisma = new Prisma({
 //   console.log(JSON.stringify(data, undefined, 2));
 // });
 
+// prisma.mutation
+//   .createPost(
+//     {
+//       data: {
+//         title: 'Graphql 101',
+//         body: '',
+//         published: false,
+//         author: {
+//           connect: {
+//             id: '5fb20bc424aa9a00084100bb',
+//           },
+//         },
+//       },
+//     },
+//     '{ id title body published  }'
+//   )
+//   .then((data) => {
+//     console.log(data);
+//     return prisma.query.users(null, '{ id name, posts {id title }}');
+//   })
+//   .then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+//   });
+
 prisma.mutation
-  .createPost(
+  .updatePost(
     {
       data: {
-        title: 'Graphql 101',
-        body: '',
-        published: false,
-        author: {
-          connect: {
-            id: '5fb20bc424aa9a00084100bb',
-          },
-        },
+        published: true,
+        body: 'new body for graphql 101',
+      },
+      where: {
+        id: '5fb5e1d3a7b11b00070af8b3',
       },
     },
-    '{ id title body published  }'
+    '{ id title }'
   )
   .then((data) => {
     console.log(data);
-    return prisma.query.users(null, '{ id name, posts {id title }}');
+    return prisma.query.posts(null, '{id  title body published}');
   })
   .then((data) => {
     console.log(JSON.stringify(data, undefined, 2));
