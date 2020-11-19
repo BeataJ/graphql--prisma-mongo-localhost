52c6a1e01cd5cfe90cdbbac8f09d207a9e4a66e8
beata <beata@emc2wave.com>
2020-11-18 18:49:45 -0800

    create post prisma mutation

--- src/prisma.js
+++ src/prisma.js
@@ -12,3 +12,23 @@ const prisma = new Prisma({
 // prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
 //   console.log(JSON.stringify(data, undefined, 2));
 // });
+
+prisma.mutation
+  .createPost(
+    {
+      data: {
+        title: 'new graphql post live',
+        body: 'You can find new course here',
+        published: true,
+        author: {
+          connect: {
+            id: '5fb20bc424aa9a00084100bb',
+          },
+        },
+      },
+    },
+    '{ id title body published  }'
+  )
+  .then((data) => {
+    console.log(data);
+  });
