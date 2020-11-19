19abcc1c8ceace77f6c3fea09ca61e7203ed27b0
beata <beata@emc2wave.com>
2020-11-18 19:11:09 -0800

    add promise channing createPost query users

--- src/prisma.js
+++ src/prisma.js
@@ -5,30 +5,34 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466/',
 });
 
-prisma.query.users(null, '{ id name posts { id title} }').then((data) => {
-  console.log(JSON.stringify(data, undefined, 2));
-});
+// prisma.query.users(null, '{ id name posts { id title} }').then((data) => {
+//   console.log(JSON.stringify(data, undefined, 2));
+// });
 
 // prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
 //   console.log(JSON.stringify(data, undefined, 2));
 // });
 
-// prisma.mutation
-//   .createPost(
-//     {
-//       data: {
-//         title: 'new graphql post live',
-//         body: 'You can find new course here',
-//         published: true,
-//         author: {
-//           connect: {
-//             id: '5fb20bc424aa9a00084100bb',
-//           },
-//         },
-//       },
-//     },
-//     '{ id title body published  }'
-//   )
-//   .then((data) => {
-//     console.log(data);
-//   });
+prisma.mutation
+  .createPost(
+    {
+      data: {
+        title: 'Graphql 101',
+        body: '',
+        published: false,
+        author: {
+          connect: {
+            id: '5fb20bc424aa9a00084100bb',
+          },
+        },
+      },
+    },
+    '{ id title body published  }'
+  )
+  .then((data) => {
+    console.log(data);
+    return prisma.query.users(null, '{ id name, posts {id title }}');
+  })
+  .then((data) => {
+    console.log(JSON.stringify(data, undefined, 2));
+  });
